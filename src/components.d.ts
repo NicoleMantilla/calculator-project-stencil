40d48dec810e26c3956b12cd7c6081065a3efc04
John Doe <nicolemf14@gmail.com>
2024-02-04 22:24:10 +0100

    update: calculator-component with the functions.

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,6 +7,7 @@
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
     interface CalculatorComponent {
+        "calculatorButtons": string[];
     }
 }
 declare global {
@@ -22,6 +23,7 @@ declare global {
 }
 declare namespace LocalJSX {
     interface CalculatorComponent {
+        "calculatorButtons"?: string[];
     }
     interface IntrinsicElements {
         "calculator-component": CalculatorComponent;
--- src/components/calculator-component/calculator-component.tsx
+++ src/components/calculator-component/calculator-component.tsx
@@ -1,23 +1,96 @@
-import { Component,h } from "@stencil/core";
+import { Component, Prop, State, h } from '@stencil/core';
+
+const deleteLabel = 'DELETE';
+const equalLabel = '=';
+const resetLabel = 'C';
+
+const faultyOperation = 'faultyOperation';
+
 @Component({
-  tag:'calculator-component',
+  tag: 'calculator-component',
   styleUrl: 'calculator-component.css',
   shadow: true,
 })
 export class CalculatorComponent {
+  @Prop() calculatorButtons: string[] = [];
+  @State() currentOperation: string = '';
+  @State() operationResult: string = '';
+  @State() history: string[] = [];
 
+  private defaultCalculatorButtons = ['1', '2', '3', '/', '4', '5', '6', '*', '7', '8', '9', '-', '0', '.', equalLabel, '+', deleteLabel, resetLabel];
 
-	render() {
-	  return (
-		<div class="b-calculator-body">
-		  <div class= "calculator-container">
-			<div class="calculator-result-preview">
-			  <div id="operation">12*4</div>
-			 <div id= "operation-result">48</div>
-			</div>
-		  </div>
-  
-		</div>
-	  );
-	}
+  render() {
+    const buttons = this.calculatorButtons.length > 0 ? this.calculatorButtons : this.defaultCalculatorButtons;
+    console.log('high level', this.history);
+    return (
+      <div class="b-calculator-body flex-center">
+        <div class="calculator-container">
+          <div class="calculator-result-preview">
+            <div class="operation">{this.currentOperation}</div>
+            <div class="operation-result">{this.operationResult}</div>
+            <div class="calculator-buttons">
+              {buttons.map(button => (
+                <calculator-button label={button} handleClick={() => this.handleButtonClick(button)} widthAuto={button === deleteLabel ? true : false} />
+              ))}
+            </div>
+          </div>
+        </div>
+        <calculator-history entries={this.history} />
+      </div>
+    );
   }
+
+  handleButtonClick(button: string) {
+    switch (button) {
+      case equalLabel:
+        this.calculateResult();
+        break;
+      case deleteLabel:
+        this.deleteLastCharacter();
+        break;
+      case resetLabel:
+        this.resetOperation();
+        break;
+      default:
+        this.addToOperation(button);
+        break;
+    }
+  }
+
+  calculateResult() {
+    try {
+      const result = new Function('return ' + this.currentOperation)();
+      const entry = `${this.currentOperation} = ${result}`;
+      this.history = [...this.history, entry];
+      this.currentOperation = '';
+      this.operationResult = result.toString();
+    } catch (error) {
+      this.currentOperation = faultyOperation;
+      this.operationResult = '';
+      this.addToHistory();
+    }
+  }
+
+  deleteLastCharacter() {
+    this.currentOperation = this.currentOperation.slice(0, -1);
+    this.operationResult = '';
+  }
+
+  resetOperation() {
+    this.currentOperation = '';
+    this.operationResult = '';
+  }
+
+  addToOperation(button: string) {
+    this.currentOperation += button;
+    this.operationResult = '';
+  }
+
+  addToHistory() {
+    if (this.currentOperation !== faultyOperation) {
+      const entry = `${this.currentOperation} = ${this.operationResult}`;
+      this.history = [...this.history, entry];
+    }
+    this.resetOperation();
+  }
+}
