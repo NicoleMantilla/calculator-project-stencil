881cffb32f6d97329099d44ace987e71d7c676c4
John Doe <nicolemf14@gmail.com>
2024-02-04 22:02:27 +0100

    add title.

---
+++ src/components/calculator-component/calculator-component.tsx
@@ -0,0 +1,14 @@
+import { Component,h } from "@stencil/core";
+
+@Component({
+	tag:'calculator-component'
+})
+export class CalculatorComponent {
+	render (){
+		return(			
+		<div class= "b-calculator-box">
+			<h1>CALCULATOR</h1>
+		</div>	
+		)
+	}
+}
